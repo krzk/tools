5d344b46c11b6b7807e86ef5292c82d66dd4db7e
Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
2015-08-26 21:26:14 +0900

    Add odroid-serial.sh
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>

---
+++ odroid-serial.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Copyright (c) 2015 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+picocom -b 115200 /dev/ttyUSB0
