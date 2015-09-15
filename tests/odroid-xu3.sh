3259c9e7ce31a77dce1d7190449438327ac250e5
Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
2015-09-15 23:17:49 +0900

    tests: odroid-xu3: Enable new tests: board name and CPU MMC stress
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>

--- tests/odroid-xu3.sh
+++ tests/odroid-xu3.sh
@@ -7,25 +7,30 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 #
-
-set -e -E
+#set -e -E
 
 . $(dirname ${BASH_SOURCE[0]})/inc/0-common.sh
 . $(dirname ${BASH_SOURCE[0]})/inc/odroid-xu3-cpu-online.sh
 . $(dirname ${BASH_SOURCE[0]})/inc/odroid-xu3-thermal.sh
 . $(dirname ${BASH_SOURCE[0]})/inc/rtc.sh
+. $(dirname ${BASH_SOURCE[0]})/inc/odroid-xu3-board-name.sh
+. $(dirname ${BASH_SOURCE[0]})/inc/odroid-xu3-cpu-mmc-stress.sh
 
-test_cpu_online 6
+test_board_name
+test_cpu_online 8
 test_thermal
 test_rtc
+test_cpu_mmc_stress
 
 # Sound: manual or:
-aplay /usr/share/sounds/alsa/Front_Right.wav > /dev/null
+sudo -u $USER aplay /usr/share/sounds/alsa/Front_Right.wav > /dev/null
 echo "Sound/aplay: OK"
 
 echo "3810000.audss-clock-controller" > /sys/bus/platform/drivers/exynos-audss-clk/unbind
 echo "3810000.audss-clock-controller" > /sys/bus/platform/drivers/exynos-audss-clk/bind
 echo "Audss rebind: OK"
+
+
 
 # Other:
 # USB: manual
