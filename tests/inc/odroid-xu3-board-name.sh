2371c82ec04c5f4764c094f7989c6cd67286cd82
Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
2015-09-15 23:17:58 +0900

    tests: odroid-xu3: Add board name and CPU MMC stress tests
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>

---
+++ tests/inc/odroid-xu3-board-name.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2015 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+. $(dirname ${BASH_SOURCE[0]})/0-common.sh
+
+test_board_name() {
+    local of="/sys/firmware/devicetree/base/compatible"
+    echo -n "Board: "
+    grep -z 'hardkernel,odroid-xu3-lite$' -q $of && echo "Odroid XU3 Lite" && return 0
+    grep -z 'hardkernel,odroid-xu3$' -q $of && echo "Odroid XU3" && return 0
+    grep -z 'hardkernel,odroid-xu4$' -q $of && echo "Odroid XU4" && return 0
+    echo
+    echo "ERROR: Wrong board"
+}
---
+++ tests/inc/odroid-xu3-cpu-mmc-stress.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright (c) 2015 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+. $(dirname ${BASH_SOURCE[0]})/0-common.sh
+
+# grep . /sys/class/thermal/*/temp
+
+test_cpu_mmc_stress() {
+    local name="CPU stress"
+    print_msg "Starting"
+    local therm="/sys/class/thermal"
+    local t1="$(cat ${therm}/thermal_zone0/temp)"
+    local t2=""
+    local pids=""
+
+    test_cat_gt ${therm}/thermal_zone0/temp 25000
+    test_cat_lt ${therm}/thermal_zone0/temp 45000
+
+
+    for i in `seq 8`; do
+        { sudo -u $USER cat /dev/mmcblk0p2 | gzip -c > /dev/null & disown ; } 2> /dev/null
+        pids="$pids $!"
+    done
+
+    sleep 15
+
+    t2="$(cat ${therm}/thermal_zone0/temp)"
+    test $t1 -lt $t2  || print_msg "ERROR: test $t1 -lt $t2"
+
+    test_cat_gt ${therm}/thermal_zone0/temp 40000
+    test_cat_lt ${therm}/thermal_zone0/temp 65000
+
+    sudo -u $USER kill $pids
+
+    print_msg "Temperature diff: $(expr $t2 - $t1)"
+}
