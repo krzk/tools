4870bc7d51ee535dd27255d7a644e4aae4ea1a86
Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
2015-10-18 21:26:08 +0900

    krzk-check-remote: New helper for syncing with remote
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>

---
+++ krzk-check-remote.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Copyright (c) 2015 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+BRANCHES="defconfig-for-next dt-for-next dt64-for-next fixes for-next samsung-for-next"
+
+die() {
+	echo "Fail: $1"
+	exit 1
+}
+
+for br in $BRANCHES; do
+	echo -n "${br}: "
+	# git checkout $br || die "git checkout $br error"
+	git rev-list --count ${br}..${br}@{u}
+	test $? -eq 0 || die "git rev-list error"
+done
+
+echo "Proceed with git reset --hard? [y/n]"
+read ANSWER
+
+test "$ANSWER" = "y" || exit
+
+for br in $BRANCHES; do
+	BR_COMMITS=`git rev-list --count ${br}..${br}@{u}`
+	test $? -eq 0 || die "git rev-list error"
+	if [ $BR_COMMITS -gt 0 ]; then
+		echo "Updating: $br from $(git rev-parse ${br}) to $(git rev-parse ${br}@{u})"
+		git checkout $br || die "git checkout $br error"
+		git reset --hard ${br}@{u}
+	fi
+done
+
