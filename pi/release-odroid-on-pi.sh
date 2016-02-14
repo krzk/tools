abc111e82a4c7d37bd9aa3f14b516e62d0d7040c
Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
2016-02-14 17:53:32 +0900

    pi/release-odroid-on-pi: Accept -c config argument
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>

--- pi/release-odroid-on-pi.sh
+++ pi/release-odroid-on-pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2015 Krzysztof Kozlowski
+# Copyright (c) 2015,2016 Krzysztof Kozlowski
 # Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
@@ -17,16 +17,32 @@ KBUILD_OUTPUT="out/"
 BINS="${KBUILD_OUTPUT}zImage ${KBUILD_OUTPUT}arch/arm/boot/dts/exynos5422-odroidxu3-lite.dtb"
 MODULES_OUT="modules-out"
 PI_REMOTE=0
-test "$1" == "-r" && PI_REMOTE=1
+CONFIG_NAME="exynos"
+
+while getopts "rc:" flag
+do
+	case "$flag" in
+		r)
+			PI_REMOTE=1
+			;;
+		c)
+			CONFIG_NAME="$OPTARG"
+			;;
+		*)
+			echo "Usage: $(basename $0) [-r] [-c config_name]"
+			exit 127
+			;;
+	esac
+done
 
 rm -fr $BINS $MODULES_OUT
 
 echo "##############################################"
 echo "Executing:"
-echo ~/dev/tools/release.sh -c exynos -E IPV6 -t tests -m $MODULES_OUT
+echo ~/dev/tools/release.sh -c $CONFIG_NAME -t tests -m $MODULES_OUT
 echo "##############################################"
 echo
-~/dev/tools/release.sh -c exynos -E IPV6 -t tests -m $MODULES_OUT || die "release fail"
+~/dev/tools/release.sh -c $CONFIG_NAME -m $MODULES_OUT || die "release fail"
 
 for file in $BINS; do
 	test -f "$file" || die "No $file"
