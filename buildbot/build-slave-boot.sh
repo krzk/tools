5669a638f5666290953749655db3758e1cce4290
Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
2016-02-23 21:52:46 +0900

    buildbot/build-slave-boot/test: Kill all serial loggers, refactor
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>

--- buildbot/build-slave-boot.sh
+++ buildbot/build-slave-boot.sh
@@ -26,6 +26,27 @@ SSH_WAIT_FOR_BOOT_TRIES=100
 SERIAL=/dev/ttyUSB0
 LOG_FILE=serial.log
 
+kill_old_log_serial() {
+	local existing="$(ps -C ts -o pid --no-headers)"
+	for pid in $existing; do
+		echo "Killing existing instance PID $pid of 'ts'"
+		kill $pid &> /dev/null
+		kill -9 $pid &> /dev/null
+	done
+}
+
+# log_serial target serial log_file
+# Echos the PID of logging process
+log_serial() {
+	local target=$1
+	local serial=$2
+	local log_file=$3
+
+	stty -F $serial 115200 cs8 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+	ts < $serial > $log_file &
+	echo $!
+}
+
 # wait_for_ping_die target
 wait_for_ping_die() {
 	local target=$1
@@ -108,10 +129,9 @@ wait_for_boot() {
 echo "Rebooting target ${TARGET}..."
 reboot_target $TARGET
 
+kill_old_log_serial
 echo "Collecting logs in background from ${TARGET}..."
-stty -F $SERIAL 115200 cs8 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
-ts < $SERIAL > $LOG_FILE &
-LOG_PID=$!
+LOG_PID=$(log_serial $TARGET $SERIAL $LOG_FILE)
 
 echo "Wait for boot of ${TARGET}..."
 wait_for_boot $TARGET
--- buildbot/build-slave-test.sh
+++ buildbot/build-slave-test.sh
@@ -25,6 +25,27 @@ TIMEOUT=3
 SERIAL=/dev/ttyUSB0
 LOG_FILE=serial.log
 
+kill_old_log_serial() {
+	local existing="$(ps -C ts -o pid --no-headers)"
+	for pid in $existing; do
+		echo "Killing existing instance PID $pid of 'ts'"
+		kill $pid &> /dev/null
+		kill -9 $pid &> /dev/null
+	done
+}
+
+# log_serial target serial log_file
+# Echos the PID of logging process
+log_serial() {
+	local target=$1
+	local serial=$2
+	local log_file=$3
+
+	stty -F $serial 115200 cs8 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+	ts < $serial > $log_file &
+	echo $!
+}
+
 ssh_works() {
 	ssh -o "ConnectTimeout $TIMEOUT" $SSH_TARGET id &> /dev/null
 }
@@ -39,10 +60,9 @@ run_tests() {
 	set +e +E
 }
 
+kill_old_log_serial
 echo "Collecting logs in background from ${TARGET}..."
-stty -F $SERIAL 115200 cs8 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
-ts < $SERIAL > $LOG_FILE &
-LOG_PID=$!
+LOG_PID=$(log_serial $TARGET $SERIAL $LOG_FILE)
 
 run_tests $TARGET
 
