48fc1e35d08c4ff1450d6b18a8e42b1278538e5f
Krzysztof Kozlowski <krzk@kernel.org>
2016-03-18 22:16:13 +0900

    repos-housekeeping: Common task for directory with many linux trees
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

---
+++ repos-housekeeping.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Copyright (c) 2016 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#                             <krzk@kernel.org>
+#
+# Should be run inside directory with repos
+
+for i in `ls`
+do
+	test -d "$i" || continue
+	cd "$i"
+
+	git rev-parse --is-inside-work-tree > /dev/null 2>&1
+	if [ $? -ne 0  ]; then
+		cd ..
+		continue
+	fi
+
+	echo "Prunning remotes in repo $i"
+	git remote prune `git remote`
+
+	echo "Running gc in repo $i"
+	git gc
+
+	cd ..
+done
