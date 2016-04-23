a6fa54b80dd5bb618e0c1ccef5bb9dc293df7d38
Krzysztof Kozlowski <krzk@kernel.org>
2016-04-23 19:35:04 +0200

    tests-var/s5p-sss: Do the testing in a loop
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests-var/s5p-sss.sh
+++ tests-var/s5p-sss.sh
@@ -7,4 +7,11 @@ cat /proc/crypto
 dmesg | grep alg
 dmesg | grep s5p
 
-sudo modprobe tcrypt sec=1 mode=500
+modprobe tcrypt sec=1 mode=500
+
+for i in `seq 10`; do
+	echo "Round $i ############"
+	modprobe tcrypt sec=1 mode=500
+	echo "Round $i ############"
+	sleep 3
+done
