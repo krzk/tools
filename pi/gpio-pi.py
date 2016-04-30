0f4d89090764549225b338c6c9d8867e501379e2
Krzysztof Kozlowski <krzk@kernel.org>
2016-04-30 19:32:30 +0200

    pi/gpio-pi.py: Parametrize per target GPIO number
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- pi/gpio-pi.py
+++ pi/gpio-pi.py
@@ -7,30 +7,32 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 #
-# TODO: parametrize GPIO number
-#
 
 import RPi.GPIO as GPIO
 import sys
 import time
 
-pin = 2
+def target_to_pin(target):
+    pin = 0
+    if (target == "odroidxu3"):
+        pin = 2
+    return pin
 
 def gpio_setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
 
-def gpio_on():
+def gpio_on(pin):
     print("Turning on...")
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
 
-def gpio_off():
+def gpio_off(pin):
     print("Turning off...")
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.HIGH)
 
-def gpio_status():
+def gpio_status(pin):
     status = "on"
     direction = GPIO.gpio_function(pin)
 
@@ -47,26 +49,32 @@ def gpio_status():
     print("Current GPIO" + str(pin) + ": " + status)
 
 def print_help():
-    print("Usage: " + str(sys.argv[0]) + " <command>")
+    print("Usage: " + str(sys.argv[0]) + " <target> <command>")
+    print("   target:  odroidxu3, odroidu3, odroidxu")
     print("   command: on, off, restart, status")
     sys.exit()
 
 def main():
+    pin = 0
     if (len(sys.argv) != 3):
         print_help()
 
+    pin = target_to_pin(sys.argv[1])
+    if (pin == 0):
+        raise ValueError("Could not match pin for given target: '" + str(sys.argv[1]) + "'")
+
     gpio_setup()
 
-    if (sys.argv[1] == "on"):
-        gpio_on()
-    elif (sys.argv[1] == "off"):
-        gpio_off()
-    elif (sys.argv[1] == "restart"):
-        gpio_off()
+    if (sys.argv[2] == "on"):
+        gpio_on(pin)
+    elif (sys.argv[2] == "off"):
+        gpio_off(pin)
+    elif (sys.argv[2] == "restart"):
+        gpio_off(pin)
         time.sleep(2)
-        gpio_on()
-    elif (sys.argv[1] == "status"):
-        gpio_status()
+        gpio_on(pin)
+    elif (sys.argv[2] == "status"):
+        gpio_status(pin)
     else:
         print_help()
 
