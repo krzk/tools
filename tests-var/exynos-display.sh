2639be4f541bea597155da7ff1376963cd0dcb9b
Krzysztof Kozlowski <krzk@kernel.org>
2016-05-02 21:55:12 +0200

    tests-var/exynos-display: Add modetest
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests-var/exynos-display.sh
+++ tests-var/exynos-display.sh
@@ -7,3 +7,23 @@ grep . /sys/class/drm/card0-*/*
 killall Xorg
 echo 3 > /sys/devices/platform/exynos-drm/graphics/fb0/blank
 echo 0 > /sys/devices/platform/exynos-drm/graphics/fb0/blank
+
+# modetest
+# git://anongit.freedesktop.org/mesa/drm
+# set -e -E
+# rm -fr out/
+# make clean -j4
+# ./autogen.sh --enable-exynos-experimental-api --enable-install-test-programs --enable-static --host=arm-linux-gnueabi --prefix=/tmp/drm-install/
+# make -j4
+# make install
+# tar -cf drm-install-arm.tar /tmp/drm-install/
+
+./kmstest
+./modetest -e
+./modetest -c
+./modetest -f
+./modetest -p
+# Odroid XU3:
+./modetest -s 28:1920x1080
+./modetest -s 28:1920x1080 -v
+./modetest -s 28:1920x1080 -v -C
