24e4e0c846cae97d916e76405acd3eb505cacab5
Krzysztof Kozlowski <krzk@kernel.org>
2016-05-07 14:10:16 +0200

    pi/serial: Fix if-elif and string comparisons
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- pi/serial.sh
+++ pi/serial.sh
@@ -10,11 +10,11 @@
 
 SERIAL=/dev/ttyUSB
 if [ $# -gt 0 ]; then
-	if [ "$1" == "xu3" ]; then
+	if [ "$1" = "xu3" ]; then
 		SERIAL="/dev/serial/by-id/usb-Silicon_Labs_CP2104_USB_to_UART_Bridge_Controller_00CFE461-if00-port0"
-	if [ "$1" == "u3" ]; then
+	elif [ "$1" = "u3" ]; then
 		SERIAL="/dev/serial/by-id/usb-Silicon_Labs_CP2104_USB_to_UART_Bridge_Controller_00DC74F5-if00-port0"
-	if [ "$1" == "xu" ]; then
+	elif [ "$1" = "xu" ]; then
 		SERIAL="/dev/serial/by-id/usb-Silicon_Labs_CP2104_USB_to_UART_Bridge_Controller_008F8288-if00-port0"
 	else
 		SERIAL="${SERIAL}$1"
