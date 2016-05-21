614e538f17cdb7b510a74b6ce65896157df24788
Krzysztof Kozlowski <krzk@kernel.org>
2016-05-21 14:34:10 +0200

    tests: Add various uncategorized tests like udevadm trigger
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

---
+++ tests/var-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright (c) 2016 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+set -e -E
+. $(dirname ${BASH_SOURCE[0]})/inc-common.sh
+
+test_var() {
+	local name="Various"
+
+	udevadm trigger
+
+	print_msg "OK"
+}
+
+test_var
