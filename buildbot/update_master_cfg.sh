e805c0dbb863bff3c221626caa2bdedae39c842a
Krzysztof Kozlowski <krzk@kernel.org>
2016-11-05 20:06:55 +0200

    Update my email address to kernel.org
    For all of my development work I prefer kernel.org. Leave the Gmail
    because it is the one tied to my identity.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- buildbot/update_master_cfg.sh
+++ buildbot/update_master_cfg.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 # Copyright (c) 2016 Krzysztof Kozlowski
-# Author: Krzysztof Kozlowski <krzk@kernel.org>
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#                             <krzk@kernel.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
--- tests/odroid-xu3-board-name.sh
+++ tests/odroid-xu3-board-name.sh
@@ -2,6 +2,7 @@
 #
 # Copyright (c) 2015 Krzysztof Kozlowski
 # Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#                             <krzk@kernel.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
