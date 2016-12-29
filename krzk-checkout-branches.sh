1143c2a9750862f6d90ba826874dd60385236e46
Krzysztof Kozlowski <krzk@kernel.org>
2016-12-29 15:22:27 +0200

    Add helper for checking out all branches on fresh clone
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

---
+++ krzk-checkout-branches.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Copyright (c) 2016 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#                             <krzk@kernel.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+BRANCHES="next/defconfig next/defconfig64 next/drivers next/dt next/dt64 fixes for-next next/soc next/soc64"
+REMOTE="krzk-korg"
+
+die() {
+	echo "Fail: $1"
+	exit 1
+}
+
+echo "Proceed with git checkout -B? [y/n]"
+read ANSWER
+
+test "$ANSWER" = "y" || exit
+
+for br in $BRANCHES; do
+	echo "Updating: $br to ${REMOTE}/${br}"
+	git checkout -B $br ${REMOTE}/${br} || die "git checkout $br error"
+done
+
