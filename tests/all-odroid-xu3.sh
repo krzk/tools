a10df434e25153e3ee67fdad57bf4e16629a0b52
Krzysztof Kozlowski <krzk@kernel.org>
2017-03-17 15:13:24 +0200

    tests: odroid-xu3: Put RTC test at the end
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests/all-odroid-xu3.sh
+++ tests/all-odroid-xu3.sh
@@ -15,13 +15,14 @@ set -e -E -x
 . $(dirname ${BASH_SOURCE[0]})/odroid-xu3-pwm-fan.sh
 . $(dirname ${BASH_SOURCE[0]})/odroid-xu3-cpu-online.sh
 . $(dirname ${BASH_SOURCE[0]})/odroid-xu3-thermal.sh
-. $(dirname ${BASH_SOURCE[0]})/rtc.sh
 . $(dirname ${BASH_SOURCE[0]})/odroid-xu3-board-name.sh
 . $(dirname ${BASH_SOURCE[0]})/odroid-xu3-cpu-mmc-stress.sh
 . $(dirname ${BASH_SOURCE[0]})/usb.sh
 . $(dirname ${BASH_SOURCE[0]})/var-all.sh
 . $(dirname ${BASH_SOURCE[0]})/clk-s2mps11.sh
 . $(dirname ${BASH_SOURCE[0]})/audio.sh
+# RTC often fail on NFS root so put it at the end
+. $(dirname ${BASH_SOURCE[0]})/rtc.sh
 # RNG does not work on Odroid, configured in secure mode?
 #. $(dirname ${BASH_SOURCE[0]})/rng-exynos.sh
 
