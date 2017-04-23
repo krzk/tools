783dbf94e306ebe772fb883a62465f7ae91cacc8
Krzysztof Kozlowski <krzk@kernel.org>
2017-04-23 19:02:51 +0200

    run-qemu: Add virtio-net network device for vexpress
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- run-qemu.sh
+++ run-qemu.sh
@@ -30,6 +30,7 @@ CMDLINE="console=ttyS0 earlyprintk root=PARTUUID=6efc8dd5-01 rootwait rw"
 KERNEL=${ROOT_DIR}cur-linux/zImage
 INITRD=${ROOT_DIR}armv7-odroidu3-exynos-v4.10-initramfs.cpio.gz
 IMG_FILE="${ROOT_DIR}arch-arm.qcow2"
+NET=""
 MEM=1024
 CPU=2
 QEMU=arm-softmmu/qemu-system-arm
@@ -58,6 +59,7 @@ case $MACHINE in
 		DTB=${ROOT_DIR}cur-linux/dts/vexpress-v2p-ca9.dtb
 		CMDLINE_CONSOLE="console=ttyAMA0,115200"
 		IMG="-drive file=${IMG_FILE},if=none,cache=writeback,id=mmc0 -device virtio-blk-device,drive=mmc0"
+		NET="-netdev user,id=mynet0,net=192.168.76.0/24,dhcpstart=192.168.76.10 -device virtio-net-device,netdev=mynet0"
 		;;
 	smdkc210)
 		IMG="-drive file=${IMG_FILE},if=sd,bus=0,index=2"
@@ -72,4 +74,5 @@ $QEMU -m $MEM -M $MACHINE -smp $CPU $IMG -append "$CMDLINE_CONSOLE $CMDLINE" \
 	-d guest_errors \
 	-serial stdio \
 	-D ${ROOT_DIR}log-${MACHINE}.log \
+	$NET \
 	-kernel $KERNEL -dtb $DTB -initrd $INITRD
