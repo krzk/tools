efc8b0ddf189240444d3bcf3105c1022c464856c
Krzysztof Kozlowski <krzk@kernel.org>
2017-04-23 09:24:36 +0200

    qemu: Add mount and umount QCOW2 helpers
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

---
+++ umount-qemu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (c) 2017 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#                             <krzk@kernel.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+die() {
+	echo "Fail: $1"
+	exit 1
+}
+
+usage() {
+	echo "$(basename $0) <qcow2.img> <dst>"
+	exit 1
+}
+
+test $# -eq 2 || usage
+
+FILE="$1"
+test -f "$FILE" || die "Missing file $FILE"
+
+DST="$2"
+test -d "$DST" || die "Missing directory dst $DST"
+
+QEMU_PATH="out"
+QEMU_NBD="${QEMU_PATH}/qemu-nbd"
+
+umount "$DST"
+
+$QEMU_NBD -d "$FILE"
