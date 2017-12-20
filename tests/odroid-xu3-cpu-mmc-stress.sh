c54c5b34c166df9196cda85b4a855d7dc332a933
Krzysztof Kozlowski <krzk@kernel.org>
2017-12-20 21:51:24 +0100

    tests: odroid-xu3-cpu-mmc-stress: Adjust for HC1 and fix XU3 thresholds
    Adjust starting temperature to match cold winter in Duebendorf (zone0:
    30 degrees on HC1, 32 degrees of C on XU3) for Odroid HC1 and XU3-Lite.
    The final temperature also has to be risen because on recent linux-next
    the Odroid XU3 heats up to 82 degrees of C.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests/odroid-xu3-cpu-mmc-stress.sh
+++ tests/odroid-xu3-cpu-mmc-stress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2015 Krzysztof Kozlowski
+# Copyright (c) 2015-2017 Krzysztof Kozlowski
 # Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
 #                             <krzk@kernel.org>
 #
@@ -37,17 +37,18 @@ test_cpu_mmc_stress() {
 	print_msg "Using $mmc device"
 
 	if [ -d "${hwmon}/pwm1" ]; then
-		# On older multi_v7 it may not be enabled
+		# On older multi_v7 it may not be enabled.
+		# Odroid HC1 does not have it.
 		test_cat ${hwmon}/name "pwmfan"
 		test_cat ${hwmon}/pwm1 "0"
 	fi
 
-	# Make all CPUs busy
+	# Enable all CPUs and check if they have sensible temperature
 	test -f /sys/kernel/bL_switcher/active && echo 0 > /sys/kernel/bL_switcher/active
-	test_cat_gt ${therm}/thermal_zone0/temp 35000
+	test_cat_gt ${therm}/thermal_zone0/temp 25000
 	test_cat_lt ${therm}/thermal_zone0/temp 65000
 
-
+	# Make all CPUs busy
 	for i in `seq 8`; do
 		{ sudo -u $USER cat "$mmc" | gzip -c > /dev/null & disown ; } 2> /dev/null
 		pids="$pids $!"
@@ -58,8 +59,8 @@ test_cpu_mmc_stress() {
 	t2="$(cat ${therm}/thermal_zone0/temp)"
 	test $t1 -lt $t2  || print_msg "ERROR: test $t1 -lt $t2"
 
-	test_cat_gt ${therm}/thermal_zone0/temp 30000
-	test_cat_lt ${therm}/thermal_zone0/temp 80000
+	test_cat_gt ${therm}/thermal_zone0/temp 40000
+	test_cat_lt ${therm}/thermal_zone0/temp 85000
 	# Unfortunately this may not be sufficient to reach next threshold
 	# of cooling device, so fan may be still at 0
 	# test_cat_gt ${hwmon}/pwm1 50
