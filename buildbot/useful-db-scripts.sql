1a9d51d66d3d05b83cc5a63542cc04e23445173b
Krzysztof Kozlowski <krzk@kernel.org>
2017-12-20 21:00:14 +0100

    buildbot: useful-db-scripts: Document backuping DB
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- buildbot/useful-db-scripts.sql
+++ buildbot/useful-db-scripts.sql
@@ -1,3 +1,7 @@
+# Dump entire buildbot DB:
+# mysqldump --add-drop-table --add-locks  --extended-insert --lock-tables -u buildbot -p -h localhost -P 3306 --protocol tcp buildbot > bck-db-buildbot-$(date +%Y-%m-%d).sql
+
+
 # Count older commits in next:
 SELECT COUNT(*) FROM buildbot.changes where project = 'next' and when_timestamp < unix_timestamp(DATE_SUB(now(), INTERVAL 7 DAY));
 # Get rid of them:
