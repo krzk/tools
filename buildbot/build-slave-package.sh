def55fefb9696c55d25da0ae2e7f9801a4433214
Krzysztof Kozlowski <krzk@kernel.org>
2018-01-28 17:16:44 +0100

    buildbot: build-slave-package: OUT_DIR path should end with slash always
    Remove double slash when joining OUT_DIR and path.  OUT_DIR should
    already end with slash.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- buildbot/build-slave-package.sh
+++ buildbot/build-slave-package.sh
@@ -21,24 +21,24 @@ OUT_DIR="$2"
 if [ ! -f "arch/arm/boot/dts/exynos5410-odroidxu.dts" ]; then
 	# If there is no Odroid XU DTS, then duplicate the SMDK DTB
 	echo "No Odroid XU DTS, using SMDK5410"
-	cp ${OUT_DIR}/arch/arm/boot/dts/exynos5410-smdk5410.dtb ${OUT_DIR}/arch/arm/boot/dts/exynos5410-odroidxu.dtb
+	cp ${OUT_DIR}arch/arm/boot/dts/exynos5410-smdk5410.dtb ${OUT_DIR}arch/arm/boot/dts/exynos5410-odroidxu.dtb
 fi
 if [ ! -f "arch/arm/boot/dts/exynos5422-odroidxu3-lite.dts" ]; then
 	# If there is no Odroid XU3 Lite DTS, then duplicate the XU3 DTB
 	echo "No Odroid XU3 Lite DTS, using regular XU3"
-	cp ${OUT_DIR}/arch/arm/boot/dts/exynos5422-odroidxu3.dtb ${OUT_DIR}/arch/arm/boot/dts/exynos5422-odroidxu3-lite.dtb
+	cp ${OUT_DIR}arch/arm/boot/dts/exynos5422-odroidxu3.dtb ${OUT_DIR}arch/arm/boot/dts/exynos5422-odroidxu3-lite.dtb
 fi
 if [ ! -f "arch/arm/boot/dts/exynos5422-odroidhc1.dts" ]; then
 	# If there is no Odroid HC1 DTS, then duplicate the XU4 DTB
 	echo "No Odroid HC1 DTS, using regular XU4"
-	cp ${OUT_DIR}/arch/arm/boot/dts/exynos5422-odroidxu4.dtb ${OUT_DIR}/arch/arm/boot/dts/exynos5422-odroidhc1.dtb
+	cp ${OUT_DIR}arch/arm/boot/dts/exynos5422-odroidxu4.dtb ${OUT_DIR}arch/arm/boot/dts/exynos5422-odroidhc1.dtb
 fi
 
 # Remove old modules-out
-rm -fr "${OUT_DIR}/modules-out"
+rm -fr "${OUT_DIR}modules-out"
 # Install modules into modules-out
 build-slave.sh INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=modules-out modules_install
 # Delete symlinks from modules-out
-find "${OUT_DIR}/modules-out/lib/modules/" -type l -delete
+find "${OUT_DIR}modules-out/lib/modules/" -type l -delete
 # Tar the modules-out (download cannot transfer entire directories)
-tar -czf "${OUT_DIR}/modules-out.tar.gz" -C "${OUT_DIR}/modules-out" lib/modules
+tar -czf "${OUT_DIR}modules-out.tar.gz" -C "${OUT_DIR}/modules-out" lib/modules
