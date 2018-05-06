396de4c82cf189811f87426b8ef75e762772f06d
Krzysztof Kozlowski <krzk@kernel.org>
2018-05-06 11:45:48 +0200

    tests: Add script for running all tests on Odroid U3
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

---
+++ tests/all-odroidu3.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright (c) 2015-2018 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#                             <krzk@kernel.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+set -e -E -x
+
+. $(dirname ${BASH_SOURCE[0]})/inc-common.sh
+. $(dirname ${BASH_SOURCE[0]})/odroid-xu3-pwm-fan.sh
+. $(dirname ${BASH_SOURCE[0]})/odroid-xu3-cpu-online.sh
+#. $(dirname ${BASH_SOURCE[0]})/odroid-xu3-thermal.sh
+. $(dirname ${BASH_SOURCE[0]})/odroid-xu3-board-name.sh
+#. $(dirname ${BASH_SOURCE[0]})/odroid-xu3-cpu-mmc-stress.sh
+. $(dirname ${BASH_SOURCE[0]})/s5p-sss.sh
+. $(dirname ${BASH_SOURCE[0]})/s5p-sss-cryptsetup.sh
+. $(dirname ${BASH_SOURCE[0]})/usb.sh
+. $(dirname ${BASH_SOURCE[0]})/var-all.sh
+. $(dirname ${BASH_SOURCE[0]})/clk-s2mps11.sh
+. $(dirname ${BASH_SOURCE[0]})/audio.sh
+# RTC often fail on NFS root so put it at the end
+. $(dirname ${BASH_SOURCE[0]})/rtc.sh
+# RNG does not work on Odroid, configured in secure mode?
+#. $(dirname ${BASH_SOURCE[0]})/rng-exynos.sh
+. $(dirname ${BASH_SOURCE[0]})/audss.sh
+
+
+# Other:
+# USB: manual
+#reboot
+#poweroff
