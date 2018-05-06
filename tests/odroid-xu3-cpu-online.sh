0931a226f54346c4c562201ae7d5548223f39d10
Krzysztof Kozlowski <krzk@kernel.org>
2018-05-06 13:38:19 +0200

    tests: odroid-xu3-cpu-online: Add support for Odroid U3
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests/odroid-xu3-cpu-online.sh
+++ tests/odroid-xu3-cpu-online.sh
@@ -12,14 +12,31 @@
 set -e -E -x
 . $(dirname ${BASH_SOURCE[0]})/inc-common.sh
 
-# test_cpu_online <expected>
+# test_cpu_online
 test_cpu_online() {
 	local name="CPU online"
 	print_msg "Testing..."
-	local expected=$1
+	local expected_cpus=0
+
+	case "$(get_board_compatible)" in
+	hardkernel,odroid-hc1|hardkernel,odroid-xu3|hardkernel,odroid-xu3-lite|hardkernel,odroid-xu4)
+		expected_cpus=8
+		;;
+	hardkernel,odroid-u3)
+		expected_cpus=4
+		;;
+	*)
+		error_msg "Wrong board"
+	esac
 
 	test -f /sys/kernel/bL_switcher/active && echo 0 > /sys/kernel/bL_switcher/active
 	cpu_online=0
+	if [ ! -r /sys/bus/cpu/devices/cpu0/online ]; then
+		# On recent kernels on U3, the CPU0 is always online
+		if [ -r /sys/bus/cpu/devices/cpu0/cpu_capacity ]; then
+			let "cpu_online+=1"
+		fi
+	fi
 	for i in /sys/bus/cpu/devices/cpu*/online; do
 		cpu_stat=$(cat $i)
 		if [ $cpu_stat -eq 1 ]; then
@@ -27,9 +44,9 @@ test_cpu_online() {
 		fi
 	done
 	print_msg "$cpu_online"
-	test $cpu_online -eq $expected || print_msg "ERROR: test $cpu_online -ne $expected"
+	test $cpu_online -eq $expected_cpus || error_msg "test $cpu_online -ne $expected_cpus"
 
 	print_msg "OK"
 }
 
-test_cpu_online 8
+test_cpu_online
