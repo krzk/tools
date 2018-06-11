4fc03c2ae45452e8fc90788630b257f55663a266
Krzysztof Kozlowski <krzk@kernel.org>
2018-06-11 18:49:09 +0200

    Use SPDX license identifiers instead of regular license note
    Use short SPDX license identifiers because:
    1. It is precise and concise.
    2. It is language neutral.
    3. It is easy and more reliable to machine process.
    4. Leads to code that is easier to read.
    5. It is a standard and can be universal. There is no need for variation.
    6. An SPDX short identifier is immutable.
    7. Easy lookups and cross-references to the SPDX License List website.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- krzk-check-remote.sh
+++ krzk-check-remote.sh
@@ -4,9 +4,7 @@
 # Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
 #                             <krzk@kernel.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
+# SPDX-License-Identifier: GPL-2.0
 #
 
 BRANCHES="next/defconfig next/defconfig64 next/drivers next/dt next/dt64 fixes for-next master next/soc next/soc64"
--- pi/update-nfs-tools.sh
+++ pi/update-nfs-tools.sh
@@ -4,9 +4,7 @@
 # Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
 #                             <krzk@kernel.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
+# SPDX-License-Identifier: GPL-2.0
 #
 
 SRV_NFS_PATH="/srv/nfs"
