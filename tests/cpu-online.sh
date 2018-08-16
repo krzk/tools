01eff462e56048e348df1345122ee8c9526158ba
Krzysztof Kozlowski <krzk@kernel.org>
2018-08-16 18:08:48 +0200

    tests: cpu-online: Simplify and fix test on mainline
    The online state from hotplug/state differs between releases.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests/cpu-online.sh
+++ tests/cpu-online.sh
@@ -18,37 +18,18 @@ test_cpu_online() {
 
 	case "$(get_board_compatible)" in
 	hardkernel,odroid-hc1|hardkernel,odroid-xu3|hardkernel,odroid-xu3-lite|hardkernel,odroid-xu4)
-		expected_cpus=8
+		expected_cpus="0-7"
 		;;
 	hardkernel,odroid-u3|hardkernel,odroid-xu)
-		expected_cpus=4
+		expected_cpus="0-3"
 		;;
 	*)
 		error_msg "Wrong board"
 	esac
 
 	test -f /sys/kernel/bL_switcher/active && echo 0 > /sys/kernel/bL_switcher/active
-	cpu_online=0
-	if [ ! -r /sys/bus/cpu/devices/cpu0/online ]; then
-		# On recent kernels on U3, the CPU0 is always online
-		# Test recent kernel by checking hotplug (v4.9) or cpu_capacity (newer)
-		if [ -r /sys/bus/cpu/devices/cpu0/hotplug/state ]; then
-			read -r cpu_stat < /sys/bus/cpu/devices/cpu0/hotplug/state
-			if [ $cpu_stat -eq 150 ]; then
-				let "cpu_online+=1"
-			fi
-		elif [ -r /sys/bus/cpu/devices/cpu0/cpu_capacity ]; then
-			let "cpu_online+=1"
-		fi
-	fi
-	for i in /sys/bus/cpu/devices/cpu*/online; do
-		read -r cpu_stat < $i
-		if [ $cpu_stat -eq 1 ]; then
-			let "cpu_online+=1"
-		fi
-	done
-	print_msg "$cpu_online"
-	test $cpu_online -eq $expected_cpus || error_msg "test $cpu_online -ne $expected_cpus"
+
+	test_cat /sys/devices/system/cpu/online "$expected_cpus"
 
 	print_msg "OK"
 }
