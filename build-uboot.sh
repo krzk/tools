0d2beedd46fd56f677a8bf156fe1eddfd4fc485c
Krzysztof Kozlowski <krzk@kernel.org>
2019-03-06 19:38:46 +0100

    build-uboot: Allow building all Samsung configs
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- build-uboot.sh
+++ build-uboot.sh
@@ -14,15 +14,26 @@ die() {
 
 usage() {
 	echo "$(basename $0) <board>"
-	echo "    board - odroid-u3, odroid-xu3, arndale-octa"
+	echo "    board - odroid-u3, odroid-xu3, arndale-octa, all"
 	exit 1
 }
+
+build() {
+	local config="$1"
+
+	echo "#########################################################"
+	echo "Building config: $config"
+	CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm make ${config}_config
+	CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm make -j4
+}
+
 set -e -E
 
 BOARD="$1"
 test -n "$BOARD" || usage
 
 CONFIG="$BOARD"
+BUILD_ALL=0
 case "$BOARD" in
 	odroid-u3)
 		CONFIG="odroid"
@@ -32,14 +43,25 @@ case "$BOARD" in
 	arndale-octa)
 		CONFIG="arndale_octa"
 		;;
+	all)
+		BUILD_ALL=1
+		CONFIG="arndale odroid odroid-xu3 origen peach-pi peach-pit s5pc210_universal s5p_goni smdk5250 smdk5420 smdkc100 smdkv310 snow trats trats2"
+		# TODO: espresso7420
+		;;
 	*)
 		usage
 		;;
 esac
 
-CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm make clean
-CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm make ${CONFIG}_config
-CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm make -j4
+if [ $BUILD_ALL -eq 0 ]; then
+	CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm make clean
+	build $CONFIG
+else
+	CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm make clean
+	for config in $CONFIG; do
+		build $config
+	done
+fi
 
 echo "U-boot fusing for SD:"
 echo "dd iflag=dsync oflag=dsync if=u-boot-dtb.bin of=/dev/mmcblk0 seek=63"
