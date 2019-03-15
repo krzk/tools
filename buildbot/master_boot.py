0b6c66a001315f191adec2a3b118ef7e553fca17
Krzysztof Kozlowski <krzk@kernel.org>
2019-03-15 21:05:31 +0100

    buildbot: master_boot: Look for unmount successes when rebooting
    Sometimes rebooting test was failing with timeout on expecting:
    
        0: 'systemd-shutdown[1]: Unmounting file systems.'
    
    Be more flexible - look for any successful unmounts messages, not that
    unmounting is started.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- buildbot/master_boot.py
+++ buildbot/master_boot.py
@@ -210,8 +210,7 @@ def pexpect_gracefull_shutdown(target, config, halt_on_failure=True, reboot=Fals
         child.expect_exact(['Stopped Login Service.', 'Stopped Network Time Synchronization.',
                             'Unmounted /home.', 'Stopped target Swap.'])
         child.expect_exact('Reached target Shutdown')
-        child.expect_exact('systemd-shutdown[1]: Unmounting file systems.')
-        child.expect_exact(['shutdown[1]: Unmounting file systems.',
+        child.expect_exact(['Reached target Unmount All Filesystems.',
                             'All filesystems unmounted.',
                             'Unmounting \\'/oldroot/sys/kernel/config\\'.',
                             'Remounting \\'/oldroot/sys/fs/cgroup/systemd\\' read-only'])
