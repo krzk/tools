471c2e10ed03dd40c25df029d60a801b01f75427
Krzysztof Kozlowski <krzk@kernel.org>
2019-09-04 20:16:52 +0200

    tests: adc-exynos: Enable and fix tests on HC1, XU and Arndale Octa
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests/adc-exynos.sh
+++ tests/adc-exynos.sh
@@ -19,12 +19,22 @@ test_adc_exynos() {
 
 	case "$(get_board_compatible)" in
 	hardkernel,odroid-hc1)
+		if is_kernel_le 5 0; then
+			print_msg "SKIPPED: older kernel, not implemented"
+			return 0
+		fi
+		adc_values[0]="0 1"
+		adc_values[3]="0 1"
 		adc_values[9]="1294 1324"
 		;;
 	hardkernel,odroid-xu3-lite)
 		adc_values[9]="367 377"
 		;;
 	hardkernel,odroid-xu)
+		if is_kernel_le 5 2; then
+			print_msg "SKIPPED: older kernel, not implemented"
+			return 0
+		fi
 		adc_values[3]="0 1"
 		# Should be around 372 but in practice higher tolerance is needed
 		adc_values[9]="365 379"
@@ -38,8 +48,14 @@ test_adc_exynos() {
 		return 0
 		;;
 	insignal,arndale-octa)
-		print_msg "Not implemented"
-		return 0
+		# adc0 and adc1 are floating on connector, others are not connected.
+		# adc9 seems to stick around 370 even though it is not connected.
+		# Still we can get something.
+		if is_kernel_le 5 1; then
+			print_msg "SKIPPED: older kernel, not implemented"
+			return 0
+		fi
+		adc_values=("1000 4000" "1000 4000" "1000 4000" "1000 4000" "1000 4000" "1000 4000" "1000 4000" "1000 4000" "1000 4000" "350 400")
 		;;
 	*)
 		error_msg "Wrong board"
