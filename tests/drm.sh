5bf46631fdeed1f41d82812ff8743ad1d59926e9
Krzysztof Kozlowski <krzk@kernel.org>
2020-02-10 20:11:35 +0100

    tests: drm: Support GPU (Panfrost) since kernel v5.6 (v5.5 next)
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests/drm.sh
+++ tests/drm.sh
@@ -16,27 +16,39 @@ test_drm() {
 	print_msg "Testing..."
 	local drm="/sys/class/drm"
 	local expected_cards=""
+	local expected_outputs=""
 	local expected_nodrm=0
 
 	case "$(get_board_compatible)" in
 	hardkernel,odroid-u3|hardkernel,odroid-xu3|hardkernel,odroid-xu3-lite|hardkernel,odroid-xu4)
-		expected_cards="card0/card0-HDMI-A-1 card0-HDMI-A-1"
+		expected_outputs="card0/card0-HDMI-A-1 card0-HDMI-A-1"
 		;;
 	hardkernel,odroid-hc1|hardkernel,odroid-xu)
+		if is_kernel_le 5 5; then
+			expected_nodrm=1
+		else
+			expected_cards="card0"
+		fi
+		;;
+	hardkernel,odroid-xu)
 		expected_nodrm=1
 		;;
 	insignal,arndale-octa)
-		expected_cards="card0/card0-HDMI-A-1 card0-HDMI-A-1"
+		expected_outputs="card0/card0-HDMI-A-1 card0-HDMI-A-1"
 		;;
 	*)
 		error_msg "Wrong board"
 	esac
 
-	for card in $expected_cards; do
+	for card in $expected_outputs; do
 		test -d "${drm}/${card}" || error_msg "Missing card ${card}"
 		test_cat "${drm}/${card}/dpms" "On"
 		test_cat "${drm}/${card}/enabled" "disabled"
 		test_cat "${drm}/${card}/status" "disconnected"
+	done
+
+	for card in $expected_cards; do
+		test -d "${drm}/${card}" || error_msg "Missing card ${card}"
 	done
 
 	if [ $expected_nodrm -eq 1 ]; then
