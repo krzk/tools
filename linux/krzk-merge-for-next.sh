67d2fc31930c12ce0eaef4c6e183f4c60d8f9535
Krzysztof Kozlowski <krzk@kernel.org>
2020-07-26 18:39:44 +0200

    linux: krzk-merge-for-next: Work with any of my repos
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- linux/krzk-merge-for-next.sh
+++ linux/krzk-merge-for-next.sh
@@ -7,12 +7,25 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 
-BRANCHES="fixes next/defconfig next/defconfig64 next/drivers next/dt next/dt64 next/soc next/soc64"
-
 die() {
 	echo "Fail: $1"
 	exit 1
 }
+
+# Get branches
+REMOTE="$(git rev-parse --abbrev-ref --symbolic-full-name master@{upstream})"
+REMOTE="${REMOTE%%/*}"
+REMOTE_URL="$(git remote get-url ${REMOTE})"
+
+if [[ $REMOTE_URL == *"/krzk/linux.git"* ]]; then
+	BRANCHES="fixes next/defconfig next/defconfig64 next/drivers next/dt next/dt64 next/soc next/soc64"
+elif [[ $REMOTE_URL == *"/krzk/linux-mem-ctrl.git"* ]]; then
+	BRANCHES=""
+elif [[ $REMOTE_URL == *"/pinctrl/samsung.git"* ]]; then
+	BRANCHES="pinctrl-fixes pinctrl-next"
+else
+	die "Unknown upstream"
+fi
 
 git checkout for-next || die "git checkout for-next error"
 git reset --hard master || die "git reset --hard master error"
