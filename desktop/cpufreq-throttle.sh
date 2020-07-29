2992d439d3599b5158dbb711f3602d1ac9e44a02
Krzysztof Kozlowski <krzk@kernel.org>
2020-07-29 17:45:08 +0200

    desktop: Add simple CPU frequency slow down helper
    These hot days my laptop goes to critical temperature (100 deg of C for
    package)...
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

---
+++ desktop/cpufreq-throttle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2020 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#                             <krzk@kernel.org>
+#
+# SPDX-License-Identifier: GPL-2.0
+#
+
+MAX_FREQ=2000000
+if [ "$1" == "slow" ]; then
+	echo "Slowing down CPUs to $MAX_FREQ Hz..."
+elif [ "$1" == "fast" ]; then
+	read -r MAX_FREQ < /sys/bus/cpu/devices/cpu0/cpufreq/cpuinfo_max_freq
+	echo "Runnig CPUs uncapped ($MAX_FREQ Hz)..."
+else
+	echo "$(basename $0) <slow|fast>"
+	exit 1
+fi
+
+for i in /sys/bus/cpu/devices/cpu* ; do
+	echo "$MAX_FREQ" > ${i}/cpufreq/scaling_max_freq
+done
