2d6d5f75ea1c4c6f6095f29b78e3b66470b52505
Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
2021-06-07 09:22:21 +0200

    ansible: add test.sh
    Test playbooks syntax.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>

---
+++ ansible/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 Canonical Ltd.
+# Author: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
+#                             <krzk@kernel.org>
+#
+# SPDX-License-Identifier: GPL-2.0
+#
+
+set -e -E
+
+TEST_DIR="$(dirname ${BASH_SOURCE[0]})"
+
+for file in ${TEST_DIR}/*yml; do
+	ansible-playbook -v --syntax-check $file
+done
+
+exit $?
