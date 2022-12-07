c572b25c2fa195d35cbc0e399df7e3744eeac4e9
Krzysztof Kozlowski <krzk@kernel.org>
2022-12-07 14:53:48 +0100

    tests-var: qcom: hdk8450: add dmic and headset recording
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests-var/qcom/test-spkr-qcom-hdk8450.sh
+++ tests-var/qcom/test-spkr-qcom-hdk8450.sh
@@ -70,10 +70,65 @@ headset_on() {
 	amixer -c 0 cset name='RX_CODEC_DMA_RX_0 Audio Mixer MultiMedia1' 1
 }
 
+dmic0_record_on() {
+	amixer -c 0 cset name='TX DEC0 MUX' MSM_DMIC
+	#amixer -c 0 cset name='TX DEC1 MUX' MSM_DMIC
+	amixer -c 0 cset name='TX DMIC MUX0' DMIC2
+	amixer -c 0 cset name='TX DMIC MUX1' DMIC3
+	amixer -c 0 cset name='TX_AIF1_CAP Mixer DEC0' 1
+	amixer -c 0 cset name='TX_AIF1_CAP Mixer DEC1' 1
+	amixer -c 0 cset name='TX_DEC0 Volume' 100
+	amixer -c 0 cset name='TX_DEC1 Volume' 100
+	amixer -c 0 cset name='MultiMedia3 Mixer TX_CODEC_DMA_TX_3' 1
+}
+
+dmic0_record_off() {
+	amixer -c 0 cset name='MultiMedia3 Mixer TX_CODEC_DMA_TX_3' 0
+	amixer -c 0 cset name='TX_AIF1_CAP Mixer DEC0' 0
+	amixer -c 0 cset name='TX DMIC MUX0' ZERO
+	amixer -c 0 cset name='TX DEC0 MUX' ZERO
+}
+
+headset_record_on() {
+	amixer -c 0 cset name='TX DEC0 MUX' SWR_MIC
+	amixer -c 0 cset name='TX SMIC MUX0' ADC1 # or 2?
+	amixer -c 0 cset name='TX_AIF1_CAP Mixer DEC0' 1
+	amixer -c 0 cset name='TX1 MODE' ADC_NORMAL
+	amixer -c 0 cset name='ADC2_MIXER Switch' 1
+	amixer -c 0 cset name='HDR12 MUX' NO_HDR12
+	amixer -c 0 cset name='ADC2 MUX' INP2
+	amixer -c 0 cset name='ADC2 Switch' 1
+	amixer -c 0 cset name='ADC2 Volume' 12
+	amixer -c 0 cset name='DEC0 MODE' ADC_DEFAULT
+	amixer -c 0 cset name='TX_DEC0 Volume' 100
+	amixer -c 0 cset name='MultiMedia3 Mixer TX_CODEC_DMA_TX_3' 1
+}
+
+headset_record_off() {
+	amixer -c 0 cset name='MultiMedia3 Mixer TX_CODEC_DMA_TX_3' 0
+	amixer -c 0 cset name='ADC2_MIXER Switch' 0
+	amixer -c 0 cset name='ADC2 Switch' 0
+	amixer -c 0 cset name='TX SMIC MUX0' 'ZERO'
+	amixer -c 0 cset name='TX_AIF1_CAP Mixer DEC0' 0
+	amixer -c 0 cset name='TX1 MODE' ADC_INVALID
+}
+
 speakers_on
 aplay -D plughw:0,5 /usr/share/sounds/alsa/Front_Center.wav
-speakers_off
+#speakers_off
 
 # Headset:
 headset_on
 aplay -D plughw:0,4 /usr/share/sounds/alsa/Front_Center.wav
+
+# Record:
+echo "Recording for 5 seconds - DMIC"
+dmic0_record_on
+arecord -D plughw:0,6 -f S16_LE -c 1 -r 48000 -d 5 out.waw
+dmic0_record_off
+aplay -D plughw:0,5 out.waw
+
+headset_record_on
+arecord -D plughw:0,6 -f S16_LE -c 1 -r 48000 -d 5 out.waw
+headset_record_off
+aplay -D plughw:0,5 out.waw
