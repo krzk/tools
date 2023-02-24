38ee9eb9f3895c27411fc936b5d0b56bc74d514c
Krzysztof Kozlowski <krzk@kernel.org>
2023-02-24 15:18:21 +0100

    boards: alpaca: handle gracefully missing Alpaca
    Just ignore if Alpaca went grazing.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- boards/alpaca/usr/bin/alpaca-off.sh
+++ boards/alpaca/usr/bin/alpaca-off.sh
@@ -9,6 +9,8 @@
 
 SERIAL_qrd="/dev/serial/by-id/usb-QUALCOMM_Inc._Embedded_Power_Measurement__EPM__device_6E02020620151F14-if01"
 
+test -c "$SERIAL_qrd" || test -h "$SERIAL_qrd" || exit 0
+
 # Weird, Alpaca works only via initstring, not echo to picocom's stdin
 picocom -b 115200 --echo --initstring "$(echo -ne 'usbDevicePower off\r\n')" -x 100 --flow none "$SERIAL_qrd"
 picocom -b 115200 --echo --initstring "$(echo -ne 'devicePower off\r\n')" -x 100 --flow none "$SERIAL_qrd"
