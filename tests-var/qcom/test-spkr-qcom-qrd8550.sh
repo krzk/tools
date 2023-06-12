28ba4848619fafdf09d2c9f0adcc2ebe90bd82c5
Krzysztof Kozlowski <krzk@kernel.org>
2023-06-12 11:49:53 +0200

    tests-var: qcom: spkr-qcom-qrd8550: update speaker names and mixer settings
    Final working speakers on QRD8550.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests-var/qcom/test-spkr-qcom-qrd8550.sh
+++ tests-var/qcom/test-spkr-qcom-qrd8550.sh
@@ -8,24 +8,26 @@
 set -e -E -x
 
 speakers_on() {
-	amixer -c 0 cset name='SpkrNorth PA Volume' 20
-	amixer -c 0 cset name='SpkrSouth PA Volume' 20
+	amixer -c 0 cset name='SpkrLeft PA Volume' 20
+	amixer -c 0 cset name='SpkrRight PA Volume' 20
 	amixer -c 0 cset name='WSA RX0 MUX' AIF1_PB
 	amixer -c 0 cset name='WSA RX1 MUX' AIF1_PB
 	amixer -c 0 cset name='WSA_RX0 INP0' RX0
 	amixer -c 0 cset name='WSA_RX1 INP0' RX1
 	amixer -c 0 cset name='WSA_COMP1 Switch' 1
 	amixer -c 0 cset name='WSA_COMP2 Switch' 1
-	amixer -c 0 cset name='SpkrNorth WSA MODE' 0
-	amixer -c 0 cset name='SpkrSouth WSA MODE' 0
-	amixer -c 0 cset name='SpkrNorth COMP Switch' 1
-	amixer -c 0 cset name='SpkrNorth BOOST Switch' 1
-	amixer -c 0 cset name='SpkrNorth DAC Switch' 1
-	amixer -c 0 cset name='SpkrNorth VISENSE Switch' 1
-	amixer -c 0 cset name='SpkrSouth COMP Switch' 1
-	amixer -c 0 cset name='SpkrSouth BOOST Switch' 1
-	amixer -c 0 cset name='SpkrSouth DAC Switch' 1
-	amixer -c 0 cset name='SpkrSouth VISENSE Switch' 1
+	amixer -c 0 cset name='SpkrLeft WSA MODE' 0
+	amixer -c 0 cset name='SpkrRight WSA MODE' 0
+	amixer -c 0 cset name='SpkrLeft COMP Switch' 1
+	amixer -c 0 cset name='SpkrLeft PBR Switch' 1
+	amixer -c 0 cset name='SpkrLeft BOOST Switch' 1
+	amixer -c 0 cset name='SpkrLeft DAC Switch' 1
+	amixer -c 0 cset name='SpkrLeft VISENSE Switch' 1
+	amixer -c 0 cset name='SpkrRight COMP Switch' 1
+	amixer -c 0 cset name='SpkrRight PBR Switch' 1
+	amixer -c 0 cset name='SpkrRight BOOST Switch' 1
+	amixer -c 0 cset name='SpkrRight DAC Switch' 1
+	amixer -c 0 cset name='SpkrRight VISENSE Switch' 1
 	amixer -c 0 cset name='WSA_RX0 Digital Volume' 85
 	amixer -c 0 cset name='WSA_RX1 Digital Volume' 85
 	amixer -c 0 cset name='WSA_CODEC_DMA_RX_0 Audio Mixer MultiMedia2' 1
@@ -38,12 +40,16 @@ speakers_off() {
 	amixer -c 0 cset name='WSA_COMP2 Switch' 0
 	amixer -c 0 cset name="WSA_RX0 INP0" ZERO
 	amixer -c 0 cset name="WSA_RX1 INP0" ZERO
-	amixer -c 0 cset name='SpkrNorth COMP Switch' 0
-	amixer -c 0 cset name='SpkrNorth VISENSE Switch' 0
-	amixer -c 0 cset name='SpkrNorth DAC Switch' 0
-	amixer -c 0 cset name='SpkrSouth COMP Switch' 0
-	amixer -c 0 cset name='SpkrSouth VISENSE Switch' 0
-	amixer -c 0 cset name='SpkrSouth DAC Switch' 0
+	amixer -c 0 cset name='SpkrLeft COMP Switch' 0
+	amixer -c 0 cset name='SpkrLeft PBR Switch' 0
+	amixer -c 0 cset name='SpkrLeft BOOST Switch' 0
+	amixer -c 0 cset name='SpkrLeft DAC Switch' 0
+	amixer -c 0 cset name='SpkrLeft VISENSE Switch' 0
+	amixer -c 0 cset name='SpkrRight COMP Switch' 0
+	amixer -c 0 cset name='SpkrRight PBR Switch' 0
+	amixer -c 0 cset name='SpkrRight BOOST Switch' 0
+	amixer -c 0 cset name='SpkrRight DAC Switch' 0
+	amixer -c 0 cset name='SpkrRight VISENSE Switch' 0
 }
 
 if [ -c /dev/snd/pcmC0D4p ]; then
@@ -56,4 +62,4 @@ fi
 
 speakers_on
 aplay -D plughw:0,${SPEAKER} /usr/share/sounds/alsa/Front_Center.wav
-#speakers_off
+speakers_off
