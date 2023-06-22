115b83b6ac44e79a0401fbf68b7104a0e4da1b35
Krzysztof Kozlowski <krzk@kernel.org>
2023-06-22 09:53:13 +0200

    buildbot: build-slave-package: drop old DTS compatibility
    DTS for Odroid XU, XU3-Lite and HC1 were merged long time ago, so all
    trees, even the stable, should have them.  Remove the compatibility code
    which will also simplify the code for handling new per-vendor
    subdirectories in DTS.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- buildbot/build-slave-package.sh
+++ buildbot/build-slave-package.sh
@@ -19,27 +19,6 @@ test $# -eq 2 || die "Wrong number of parameters"
 NAME="$1"
 OUT_DIR="$2"
 
-if [ ! -f "arch/arm/boot/dts/exynos5410-odroidxu.dts" ]; then
-	# If there is no Odroid XU DTS, then duplicate the SMDK DTB
-	echo "No Odroid XU DTS, using SMDK5410"
-	cp ${OUT_DIR}arch/arm/boot/dts/exynos5410-smdk5410.dtb ${OUT_DIR}arch/arm/boot/dts/exynos5410-odroidxu.dtb
-fi
-if [ ! -f "arch/arm/boot/dts/exynos5422-odroidxu3-lite.dts" ]; then
-	# If there is no Odroid XU3 Lite DTS, then duplicate the XU3 DTB
-	echo "No Odroid XU3 Lite DTS, using regular XU3"
-	cp ${OUT_DIR}arch/arm/boot/dts/exynos5422-odroidxu3.dtb ${OUT_DIR}arch/arm/boot/dts/exynos5422-odroidxu3-lite.dtb
-fi
-if [ ! -f "arch/arm/boot/dts/exynos5422-odroidhc1.dts" ]; then
-	# All compiled kernels have SMDK5410 and XU3 DTS but for example v4.1 does not have XU4 DTS
-	if [ -f "arch/arm/boot/dts/exynos5422-odroidxu4.dts" ]; then
-		# If there is no Odroid HC1 DTS, then duplicate the XU4 DTB
-		echo "No Odroid HC1 DTS, using regular XU4"
-		cp ${OUT_DIR}arch/arm/boot/dts/exynos5422-odroidxu4.dtb ${OUT_DIR}arch/arm/boot/dts/exynos5422-odroidhc1.dtb
-	else
-		echo "No Odroid HC1 DTS and no XU4 DTS."
-	fi
-fi
-
 # Remove old modules-out
 rm -fr "${OUT_DIR}modules-out"
 # Install modules into modules-out
