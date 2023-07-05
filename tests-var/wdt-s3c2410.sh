be3d00a72d75f1efdb4e384dfa3c3914b9133a63
Krzysztof Kozlowski <krzk@kernel.org>
2023-07-05 11:24:51 +0200

    tests-var: wdt-s3c2410: watchdog daemon is gone, simplify
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests-var/wdt-s3c2410.sh
+++ tests-var/wdt-s3c2410.sh
@@ -1,11 +1,15 @@
-# Copyright (c) 2017 Krzysztof Kozlowski
+# Copyright (c) 2017,2023 Krzysztof Kozlowski
 # Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
 #                             <krzk@kernel.org>
 #
 # SPDX-License-Identifier: GPL-2.0
 #
+
 # Boot with:
 setenv opts s3c2410_wdt.soft_noboot=1
-# Then kill wd:
-sudo killall -9 watchdog
+
+sudo wdctl
+
+# Assuming there is no watchdog daemon
+sudo touch /dev/watchdog0
 # Wait for timer expire
