d5642c3002f41562b508483eb6224d4dd928047c
Krzysztof Kozlowski <krzk@kernel.org>
2023-07-26 11:41:39 +0200

    buildbot: master_build_common: build all Samsung and Qcom pinctrl drivers
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

---
+++ buildbot/build-worker-add-config.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Copyright (c) 2023 Krzysztof Kozlowski
+# Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
+#                             <krzk@kernel.org>
+#
+# SPDX-License-Identifier: GPL-2.0
+#
+
+SELF_DIR="$(dirname "${BASH_SOURCE[0]}")"
+. "${SELF_DIR}/inc-build-slave.sh"
+
+set -e -E
+# Be verbose for Buildbot debugging
+set -x
+
+test $# -eq 3 || die "Wrong number of parameters"
+
+test -f arch/arm64/Kconfig.platforms || die "Missing Kconfig.platforms"
+
+OUT_DIR="$1"
+WANT_SUBSYSTEM=""
+WANT_PLATFORMS=""
+
+case "$2" in
+	pinctrl)
+		WANT_SUBSYSTEM="$2"
+		;;
+	*)
+		die "Unsupported subsystem to add"
+		;;
+esac
+
+case "$3" in
+	qcom|samsung)
+		WANT_PLATFORM="$3"
+		;;
+	*)
+		die "Unsupported platforms to add"
+		;;
+esac
+
+for f in drivers/${WANT_SUBSYSTEM}/${WANT_PLATFORM}/Kconfig* ; do
+	while IFS= read -r platform ; do
+		scripts/config --file "${OUT_DIR}.config" -e "$platform"
+	done < <(grep config -- $f | cut -d ' ' -f 2 | grep -vE "$WANT_PLATFORM")
+done
--- buildbot/master_build_common.py
+++ buildbot/master_build_common.py
@@ -349,6 +349,34 @@ def steps_build_mem_ctrl_adjust_config(builder_name, env, kbuild_output, make_ol
                                 env=env, name='Make olddefconfig'))
     return st
 
+def steps_build_pinctrl_adjust_config(builder_name, env, kbuild_output, make_olddefconfig=True):
+    st = []
+    if not env['KBUILD_OUTPUT']:
+        raise ValueError('Missing KBUILD_OUTPUT path in environment')
+    st.append(steps.ShellCommand(
+        command=['scripts/config', '--file', util.Interpolate(kbuild_output + '.config'),
+                 '-e', 'COMPILE_TEST', '-e', 'OF',
+                 '-e', 'PINCTRL'
+                ],
+        haltOnFailure=True,
+        env=env, name='Toggle pin controller compile test config'))
+    st.append(steps.ShellCommand(command=[util.Interpolate('%(prop:builddir:-~/)s/tools/buildbot/build-worker-add-config.sh'),
+                                              env['KBUILD_OUTPUT'], 'pinctrl', 'samsung'],
+                                 haltOnFailure=True,
+                                 env=env,
+                                 name='Add pinctrl/samsung to config'))
+    st.append(steps.ShellCommand(command=[util.Interpolate('%(prop:builddir:-~/)s/tools/buildbot/build-worker-add-config.sh'),
+                                              env['KBUILD_OUTPUT'], 'pinctrl', 'qcom'],
+                                 haltOnFailure=True,
+                                 env=env,
+                                 name='Add pinctrl/qcom to config'))
+
+    if make_olddefconfig:
+        st.append(steps.Compile(command=[util.Interpolate(CMD_MAKE), 'olddefconfig'],
+                                haltOnFailure=True,
+                                env=env, name='Make olddefconfig'))
+    return st
+
 def steps_build_w1_adjust_config(builder_name, env, kbuild_output, make_olddefconfig=True):
     st = []
     if not env['KBUILD_OUTPUT']:
@@ -385,6 +413,7 @@ def steps_build_w1_adjust_config(builder_name, env, kbuild_output, make_olddefco
 def steps_build_all_drivers_adjust_config(builder_name, env, kbuild_output):
      st = []
      st.extend(steps_build_mem_ctrl_adjust_config(builder_name, env, kbuild_output, make_olddefconfig=False))
+     st.extend(steps_build_pinctrl_adjust_config(builder_name, env, kbuild_output, make_olddefconfig=False))
      st.extend(steps_build_w1_adjust_config(builder_name, env, kbuild_output, make_olddefconfig=True))
      return st
 
@@ -395,14 +424,16 @@ def steps_build_selected_folders(builder_name, env):
     st.append(steps.ShellCommand(command=[util.Interpolate(CMD_MAKE), 'arch/arm/',
                                           # make won't build DTBs but include it for completeness
                                           'arch/arm64/boot/dts/',
-                                          'drivers/clk/samsung/', 'drivers/pinctrl/samsung/', 'drivers/memory/',
+                                          'drivers/clk/samsung/', 'drivers/pinctrl/samsung/',
+                                          'drivers/pinctrl/qcom/', 'drivers/memory/',
                                           'drivers/soc/samsung/', 'drivers/w1/'],
                                  haltOnFailure=True, env=env, name='Build selected paths'))
     st.append(step_touch_commit_files())
     st.append(steps.Compile(command=[util.Interpolate(CMD_MAKE), 'arch/arm/',
                                      # make won't build DTBs but include it for completeness
                                      'arch/arm64/boot/dts/',
-                                     'drivers/clk/samsung/', 'drivers/pinctrl/samsung/', 'drivers/memory/',
+                                     'drivers/clk/samsung/', 'drivers/pinctrl/samsung/',
+                                     'drivers/pinctrl/qcom/', 'drivers/memory/',
                                      'drivers/soc/samsung/', 'drivers/w1/'],
                             haltOnFailure=True,
                             warnOnWarnings=True,
