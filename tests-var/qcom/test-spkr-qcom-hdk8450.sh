a950ae6b3600c38572ebeca69f1052a78bacc118
Krzysztof Kozlowski <krzk@kernel.org>
2024-01-16 15:30:46 +0100

    tests-var: qcom: test-spkr-qcom-hdk8450: add few sleeps to avoid busy devices
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests-var/qcom/test-spkr-qcom-hdk8450.sh
+++ tests-var/qcom/test-spkr-qcom-hdk8450.sh
@@ -26,8 +26,10 @@ speakers_on() {
 	amixer -c 0 cset name='SpkrRight BOOST Switch' 1
 	amixer -c 0 cset name='SpkrRight DAC Switch' 1
 	amixer -c 0 cset name='SpkrRight VISENSE Switch' 0
+	sleep 0.1
 	amixer -c 0 cset name='WSA_RX0 Digital Volume' 85
 	amixer -c 0 cset name='WSA_RX1 Digital Volume' 85
+	sleep 0.1
 	amixer -c 0 cset name='WSA_CODEC_DMA_RX_0 Audio Mixer MultiMedia2' 1
 }
 
@@ -55,6 +57,7 @@ headset_on() {
 	amixer -c 0 cset name='RX HPH Mode' CLS_H_ULP
 	amixer -c 0 cset name='RX_MACRO RX0 MUX' AIF1_PB
 	amixer -c 0 cset name='RX_MACRO RX1 MUX' AIF1_PB
+	sleep 0.1
 	amixer -c 0 cset name='RX INT0_1 MIX1 INP0' RX0
 	amixer -c 0 cset name='RX INT1_1 MIX1 INP0' RX1
 	amixer -c 0 cset name='RX INT0 DEM MUX' CLSH_DSM_OUT
@@ -67,10 +70,13 @@ headset_on() {
 	amixer -c 0 cset name='HPHR Switch' 1
 	amixer -c 0 cset name='CLSH Switch' 1
 	amixer -c 0 cset name='LO Switch' 1
+	sleep 0.1
 	amixer -c 0 cset name='RX_CODEC_DMA_RX_0 Audio Mixer MultiMedia1' 1
 	# Must stay off:
+	sleep 0.1
 	amixer -c 0 cset name='HPHL_COMP Switch' 0
 	amixer -c 0 cset name='HPHR_COMP Switch' 0
+	sleep 0.3
 }
 
 headset_off() {
