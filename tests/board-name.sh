1c35de6a4c7876fae9a476a251d0b48362842b92
Krzysztof Kozlowski <krzk@kernel.org>
2025-02-09 20:03:45 +0100

    tests: board-name: Do not rely on grep for parsing compatible
    Yocto initramfs busybox grep does not have '-z':
    
      grep: invalid option -- 'z'
    
    so replace it with something more robust and not involving sub process.

--- tests/board-name.sh
+++ tests/board-name.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2015-2020 Krzysztof Kozlowski
+# Copyright (c) 2015-2025 Krzysztof Kozlowski
 # Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
 #                             <krzk@kernel.org>
 #
@@ -11,7 +11,7 @@ set -e -E -x
 . $(dirname "${BASH_SOURCE[0]}")/inc-common.sh
 
 test_board_name() {
-	local of="/sys/firmware/devicetree/base/compatible"
+	local compatible="$(get_board_compatible)"
 	local soc_family="Samsung Exynos"
 	local soc_id=""
 	local soc_machine=""
@@ -21,59 +21,59 @@ test_board_name() {
 
 	case "$TARGET" in
 	arndaleocta|octa)
-		grep -z 'insignal,arndale-octa$' -q $of && echo "Arndale Octa" && found_board=1
+		test "$compatible" == "insignal,arndale-octa" && echo "Arndale Octa" && found_board=1
 		soc_id="EXYNOS5420"
 		soc_machine="Insignal Arndale Octa evaluation board based on Exynos5420"
 		soc_revision="20"
 		;;
 	odroidmc1|mc1)
-		grep -z 'hardkernel,odroid-hc1$' -q $of && echo "Odroid HC1" && found_board=1
-		grep -z 'hardkernel,odroid-xu4$' -q $of && echo "Odroid XU4" && found_board=1
+		test "$compatible" == "hardkernel,odroid-hc1" && echo "Odroid HC1" && found_board=1
+		test "$compatible" == "hardkernel,odroid-xu4" && echo "Odroid XU4" && found_board=1
 		soc_id="EXYNOS5800"
 		soc_machine="Hardkernel Odroid HC1"
 		soc_revision="1"
 		;;
 	odroidu3|u3)
-		grep -z 'hardkernel,odroid-u3$' -q $of && echo "Odroid U3" && found_board=1
+		test "$compatible" == "hardkernel,odroid-u3" && echo "Odroid U3" && found_board=1
 		soc_id="EXYNOS4412"
 		soc_machine="Hardkernel ODROID-U3 board based on Exynos4412"
 		soc_revision="20"
 		;;
 	odroidx|x)
-		grep -z 'hardkernel,odroid-x$' -q $of && echo "Odroid X" && found_board=1
+		test "$compatible" == "hardkernel,odroid-x" && echo "Odroid X" && found_board=1
 		soc_id="EXYNOS4412"
 		soc_machine="Hardkernel ODROID-X board based on Exynos4412"
 		soc_revision="11"
 		;;
 	odroidxu3|xu3)
-		grep -z 'hardkernel,odroid-xu3-lite$' -q $of && echo "Odroid XU3 Lite" && found_board=1
-		grep -z 'hardkernel,odroid-xu3$' -q $of && echo "Odroid XU3" && found_board=1
+		test "$compatible" == "hardkernel,odroid-xu3-lite" && echo "Odroid XU3 Lite" && found_board=1
+		test "$compatible" == "hardkernel,odroid-xu3" && echo "Odroid XU3" && found_board=1
 		soc_id="EXYNOS5800"
 		soc_machine="Hardkernel Odroid XU3"
 		soc_revision=""
 		;;
 	odroidxu|xu)
-		grep -z 'hardkernel,odroid-xu$' -q $of && echo "Odroid XU" && found_board=1
+		test "$compatible" == "hardkernel,odroid-xu" && echo "Odroid XU" && found_board=1
 		soc_id="EXYNOS5410"
 		soc_machine="Hardkernel Odroid XU"
 		soc_revision="23"
 		;;
 	odroidhc1|hc1)
-		grep -z 'hardkernel,odroid-hc1$' -q $of && echo "Odroid HC1" && found_board=1
-		grep -z 'hardkernel,odroid-xu4$' -q $of && echo "Odroid XU4" && found_board=1
+		test "$compatible" == "hardkernel,odroid-hc1" && echo "Odroid HC1" && found_board=1
+		test "$compatible" == "hardkernel,odroid-xu4" && echo "Odroid XU4" && found_board=1
 		soc_id="EXYNOS5800"
 		soc_machine="Hardkernel Odroid HC1"
 		soc_revision="1"
 		;;
 	*)
-		grep -z 'insignal,arndale-octa$' -q $of && echo "Arndale Octa" && found_board=1
-		grep -z 'hardkernel,odroid-hc1$' -q $of && echo "Odroid HC1" && found_board=1
-		grep -z 'hardkernel,odroid-u3$' -q $of && echo "Odroid U3" && found_board=1
-		grep -z 'hardkernel,odroid-x$' -q $of && echo "Odroid X" && found_board=1
-		grep -z 'hardkernel,odroid-xu$' -q $of && echo "Odroid XU" && found_board=1
-		grep -z 'hardkernel,odroid-xu3-lite$' -q $of && echo "Odroid XU3 Lite" && found_board=1
-		grep -z 'hardkernel,odroid-xu3$' -q $of && echo "Odroid XU3" && found_board=1
-		grep -z 'hardkernel,odroid-xu4$' -q $of && echo "Odroid XU4" && found_board=1
+		test "$compatible" == "insignal,arndale-octa" && echo "Arndale Octa" && found_board=1
+		test "$compatible" == "hardkernel,odroid-hc1" && echo "Odroid HC1" && found_board=1
+		test "$compatible" == "hardkernel,odroid-u3" && echo "Odroid U3" && found_board=1
+		test "$compatible" == "hardkernel,odroid-x" && echo "Odroid X" && found_board=1
+		test "$compatible" == "hardkernel,odroid-xu" && echo "Odroid XU" && found_board=1
+		test "$compatible" == "hardkernel,odroid-xu3-lite" && echo "Odroid XU3 Lite" && found_board=1
+		test "$compatible" == "hardkernel,odroid-xu3" && echo "Odroid XU3" && found_board=1
+		test "$compatible" == "hardkernel,odroid-xu4" && echo "Odroid XU4" && found_board=1
 	esac
 
 	if is_kernel_le 5 5; then
