ec88949ec4c621d72abeee4b40ba46171eb45276
Krzysztof Kozlowski <krzk@kernel.org>
2025-02-20 18:43:53 +0100

    tests: rtc-suspend: Adjust for Busybox rtcwake
    No '-v' argument.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- tests/rtc-suspend.sh
+++ tests/rtc-suspend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2015,2016 Krzysztof Kozlowski
+# Copyright (c) 2015,2016,2025 Krzysztof Kozlowski
 # Author: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
 #                             <krzk@kernel.org>
 #
@@ -10,6 +10,17 @@
 set -e -E -x
 . $(dirname "${BASH_SOURCE[0]}")/inc-common.sh
 
+rtc_suspend_rtcwake_cmd() {
+	rtcwake --help 2>&1 | grep -q BusyBox
+	if [ $? -eq 0 ]; then
+		echo "rtcwake"
+	else
+		echo "rtcwake -v"
+	fi
+
+	return 0
+}
+
 # test_rtc_suspend_device device
 test_rtc_suspend_device() {
 	local rtc="$1"
@@ -17,7 +28,7 @@ test_rtc_suspend_device() {
 	if [ -c /dev/${rtc} ]; then
 		hwclock --systohc -f /dev/${rtc}
 		for i in `seq 3`; do
-			rtcwake -d $rtc -m mem -s 5 -v
+			$(rtc_suspend_rtcwake_cmd) -d $rtc -m mem -s 5
 			# Test whether network works after suspend:
 			sleep 5
 			ifconfig eth0
