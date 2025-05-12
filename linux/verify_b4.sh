53072c3efb85cb020ba471de2e244d36825b6aff
Krzysztof Kozlowski <krzk@kernel.org>
2025-05-12 15:42:26 +0200

    linux: verify_b4: Ignore SC2181 (check exit code)
    Does not improve readability for long commands, so ignore shellcheck:
      SC2181 (style): Check exit code directly with e.g. 'if mycmd;', not indirectly with $?.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- linux/verify_b4.sh
+++ linux/verify_b4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
 #
-# Copyright (c) 2024 Krzysztof Kozlowski
+# Copyright (c) 2024-2025 Krzysztof Kozlowski
 # Author: Krzysztof Kozlowski <krzk@kernel.org>
 #
 # Be sure people do not merge b4 branches
@@ -9,6 +9,7 @@
 which b4 > /dev/null || exit 0
 
 b4 prep --show-revision > /dev/null 2>&1
+# shellcheck disable=SC2181
 if [ $? -eq 0 ]; then
 	echo "ERROR: Looks like it is a b4-managed branch. Such branches should not be merged."
 	exit 1
