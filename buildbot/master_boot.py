4418aff003d134fc7b74939b9daf362c2160e9cb
Krzysztof Kozlowski <krzk@kernel.org>
2025-06-06 09:29:09 +0200

    buildbot: master_boot: Handle late network on multi_v7
    Ethernet driver modules are loaded very late on multi_v7, thus they are
    configured by systemd-networkd and first `ip addr` might print no IP
    configured yet:
    
      root@odroid:~# systemctl is-system-running
      running
      root@odroid:~# ip addr
      1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue qlen 1000
          link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
          inet 127.0.0.1/8 scope host lo
             valid_lft forever preferred_lft forever
          inet6 ::1/128 scope host noprefixroute
             valid_lft forever preferred_lft forever
      2: sit0@NONE: <NOARP> mtu 1480 qdisc noop qlen 1000
          link/sit 0.0.0.0 brd 0.0.0.0
      3: enu1: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc pfifo_fast qlen 1000
          link/ether 00:1e:06:30:bf:ac brd ff:ff:ff:ff:ff:ff
      root@odroid:~# [   25.623665] r8152 5-1:1.0 enu1: carrier on
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- buildbot/master_boot.py
+++ buildbot/master_boot.py
@@ -483,8 +483,13 @@ def step_check_status(target, config):
     print('Checking network online...')
     child.sendline('ip addr')
     child.expect_exact('1: lo: <LOOPBACK,UP,LOWER_UP>', timeout=5)
-    child.expect('(enu0:|enu1:|enu2u1u1:|eth0:|eth1:).*UP,LOWER_UP', timeout=5)
-    child.expect_exact('inet 192.168', timeout=5)
+    index = child.expect(['(enu0:|enu1:|enu2u1u1:|eth0:|eth1:).*UP,LOWER_UP', pexpect.TIMEOUT], timeout=5)
+    if index == 1:
+        # multi_v7 kernel loads ethernet modules very late and first `ip addr` still has no network
+        print('Retrying lookup for online network (`ip addr`)...')
+        child.sendline('ip addr')
+        child.expect(['(enu0:|enu1:|enu2u1u1:|eth0:|eth1:).*UP,LOWER_UP', pexpect.TIMEOUT], timeout=5)
+    child.expect_exact('inet 192.168', timeout=1)
     child.expect_exact('root@odroid', timeout=1)
     print('System up with network')
     """
